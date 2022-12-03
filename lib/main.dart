eec43c369bf39698b594766c066e212bef3d79f8
dev-vickie <vmutethia84@gmail.com>
2022-12-03 17:48:15 +0300

    Add splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'pages/onbording_pages/onboarding_main.dart';
+import 'pages/splash/splashpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,61 +18,3 @@ class MyApp extends StatelessWidget {
 }
 
 // Display splash for 3 sec,then navigate to the onboarding pages
-class SplashPage extends StatefulWidget {
-  const SplashPage({super.key});
-
-  @override
-  State<SplashPage> createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController fadeController;
-  late Animation animation;
-  @override
-  void initState() {
-    super.initState();
-    fadeController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-    animation = Tween(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(fadeController);
-  }
-
-  @override
-  void dispose() {
-    fadeController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    fadeController.repeat();
-    Future.delayed(
-      const Duration(seconds: 3),
-      () {
-        Navigator.pop(context);
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const OnboardingScreen(),
-          ),
-        );
-      },
-    );
-    return Container(
-      color: Colors.blue[200],
-      child: FadeTransition(
-        opacity: fadeController,
-        child: const Icon(
-          Icons.mic,
-          color: Colors.white,
-          size: 60,
-        ),
-      ),
-    );
-  }
-}
